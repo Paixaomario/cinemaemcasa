52aaa4b1413459963000e8f716c97727e249b58f
120311aepentregasrapidas <aepentregasrapidas@gmail.com>
2026-07-20 19:03:57 -0300

    feat: Adiciona PWA e corrige build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,14 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        {/* Configurações do PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body suppressHydrationWarning>
